6a79e3ba6db43b209c04f54103a1bd264680e945
korkam <korkam@users.noreply.github.com>
2021-11-17 13:46:10 -0500

    Update collection.py
    added my pi multiplier! great job!

--- collection.py
+++ collection.py
@@ -6,3 +6,8 @@ def maliniFunc(phrase):
 def samFunc(num):
     sqRoot = math.sqrt(num)
     print(sqRoot)
+
+def kkFunc(num)
+    piMult = math.pi * num
+    # Print the value of pi times num
+    print (piMult)
